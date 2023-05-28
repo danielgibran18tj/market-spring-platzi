c22202aa1abd394d4f98861ca1ed53a925f77644
danielgibran18tj <danielgibran18tj@hotmail.com>
2023-05-28 00:08:20 -0500

    realizada la estrucutra del proyecto, y configurados los properties

--- src/main/java/com/platzi/market/PlatziMarketApplication.java
+++ src/main/java/com/platzi/market/PlatziMarketApplication.java
@@ -7,6 +7,7 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 public class PlatziMarketApplication {
 
 	public static void main(String[] args) {
+
 		SpringApplication.run(PlatziMarketApplication.class, args);
 	}
 
